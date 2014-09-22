af01b28f1607363b38e14a0f30f6f774bf6fd6cd
Adam D Richman <ar@adamrichman.com>
2014-09-22 15:11:15 -0700

    tweek distance from center

--- public/javascripts/munchies-map/app.js
+++ public/javascripts/munchies-map/app.js
@@ -235,31 +235,31 @@ MunchiesMapView.prototype.pinCenter = function(path){
 };
 
 var MunchiesMap = function(mapElement, options){
-  
+
   this.renderedTrucks = { lastRequestCoords : null };
   this.googleMap      = new google.maps.Map(mapElement, options);
 
 };
 
 MunchiesMap.prototype.calculateDist = function(){
-  
+
   // http://stackoverflow.com/questions/3525670/radius-of-viewable-region-in-google-maps-v3
   var bounds = this.googleMap.getBounds();
   var center = bounds.getCenter();
   var ne     = bounds.getNorthEast();
   // r = radius of the earth in statute miles
-  var r      = 3963.0;  
+  var r      = 3963.0;
   // Convert lat or lng from decimal degrees into radians (divide by 57.2958)
-  var lat1   = center.lat() / 57.2958; 
+  var lat1   = center.lat() / 57.2958;
   var lon1   = center.lng() / 57.2958;
   var lat2   = ne.lat() / 57.2958;
   var lon2   = ne.lng() / 57.2958;
-  
+
   // distance = circle radius from center to Northeast corner of bounds
-  var dis    = r * Math.acos(Math.sin(lat1) * Math.sin(lat2) + 
+  var dis    = r * Math.acos(Math.sin(lat1) * Math.sin(lat2) +
     Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1));
-  
-  return dis / 1.3; // tweak for UX
+
+  return dis / 1.5; // tweak for UX
 };
 
 var MunchiesMapDragDelegate = function(){
